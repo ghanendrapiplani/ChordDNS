1b00cd05fc82271730492663bc403cadd232a089
BTMichalowicz <benjaminmichalowicz98@gmail.com>
2020-11-30 10:16:49 -0500

    Updated gitignore and cleaned up output files

--- src/dns_resolver/hash_func.py
+++
@@ -1,8 +0,0 @@
-# Script hash_func to be used in main.da and client.da
-#
-import hashlib
-
-def hash_func(name, m):
-	hash = int(hashlib.sha1(name.encode('utf-8')).hexdigest(), 16)
-	hash_val = hash % 2**m
-	return hash_val
