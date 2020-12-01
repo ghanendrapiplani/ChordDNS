bfe51146c568df8b98dae2af3b646058edcec80a
Ghanendra Piplani <Ghanendra.Piplani@stonybrook.edu>
2020-12-01 01:25:32 -0500

    Added support for different Hash Algorithms. Added code for runtime nodes generation. Added requirements.txt using pip freeze Added missing sections in README.md Added pyDistAlgo distribution to bundle with project for portability.

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
 # ChorDNS: DNS Resolution Analysis with a Distributed Hashtable Approach
-### Ghanendra Piplani, Tarush Abhaya Jain, Benjamin Michalowicz, Dheeraj Ramchandani
+
+## Team
+
+* Benjamin Michalowicz
+* Dheeraj Ramchandani
+* Ghanendra Piplani
+* Tarush Abhaya Jain 
 
 ## About
+
+DNS Chord implementation for CSE 534 Final Project.
+
+## Installation
+
+1. Install anaconda
+2. conda create -n py36 python=3.6
+3. conda activate py36
+4. pip install requirements.txt
+5. python ‪CSE534-ChorDNS-master/pyDistAlgo-1.0.12/setup.py install
 
 
 ## How to Run
 
+1. conda activate py36
+2. python -m da --message-buffer-size 1000000 -F output --logfile --logfilename test.log src/dns_resolver/main.da
 
 ## References
 
-## TODO:
-  1. How to run?
-  2. Getting data files to work
-  3. Plotting tests results
+## TODO
 
-
-
+1. ~~Installation~~
+2. ~~How to run?~~
+3. Getting data files to work
+4. Plotting tests results
--- src/dns_resolver/hash_func.py
+++ src/dns_resolver/hash_func.py
@@ -1,8 +1,47 @@
 # Script hash_func to be used in main.da and client.da
 #
 import hashlib
+import zlib
+import configparser
+from passlib.context import CryptContext
+
 
 def hash_func(name, m):
-	hash = int(hashlib.sha1(name.encode('utf-8')).hexdigest(), 16)
-	hash_val = hash % 2**m
-	return hash_val
+    return int(zlib.crc32(bytes(name.encode('utf-8')))) % 2 ** m
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
+    # if "adler32" in config.get("DEFAULT", "HASH_FX"):
+    #     return int(zlib.crc32(bytes(name.encode('utf-8')))) % 2 ** m
+
+    # if type == 1:
+    #     return int(hashlib.sha1(name.encode('utf-8')).hexdigest(), 16) % 2 ** m
+    # elif type == 2:
+    #     return int(hashlib.sha256(name.encode('utf-8')).hexdigest(), 16) % 2 ** m
+    # elif type == 3:
+    #     return int(zlib.adler32(bytes(name.encode('utf-8')))) % 2 ** m
+    # elif type == 4:
+
+# def hash_sha1(name, m):
+#     hash =
+#     hash_val = hash
+#     return hash_val
+#
+#
+# def hash_sha256(name, m):
+#     hash =
+#     hash_val = hash % 2 ** m
+#     return hash_val
+#
+#
+# def adler32(name, m):
+#     print("name = "+str(name))
+#     print("name = "+str(m))
+#     hash = int(zlib.adler32(bytes(name.encode('utf-8'))))
+#     hash_val = hash % 2 ** m
+#     return hash_val
+#
+#
+# def crc32(name, m):
+#     hash = int(zlib.crc32(bytes(name.encode('utf-8'))))
+#     hash_val = hash % 2 ** m
+#     return hash_val
